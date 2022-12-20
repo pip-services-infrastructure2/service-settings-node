17f5b2cf4d7a50089e88772f4aa80795c787ae1f
Dmitriy Krayniy <dkrayniy@entinco.com>
2022-12-20 12:00:43 +0400

    Added pipeline

--- protogen.ps1
+++ protogen.ps1
@@ -20,4 +20,9 @@ docker build -f "$PSScriptRoot/docker/Dockerfile.proto" -t $protosImage .
 # Create and copy compiled files, then destroy
 docker create --name $container $protosImage
 docker cp "$($container):/app/src/protos" "$PSScriptRoot/src/"
-docker rm $container
+docker rm $containerc
+
+# Verify that protos folder was indeed created after generating proto files
+if (-not (Test-Path "$PSScriptRoot/src/protos")) {
+    Write-Error "protos folder doesn't exist in src dir. Build failed. See logs above for more information."
+}
